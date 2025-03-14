9c36c439a7c612b385999433865c1e01c170b7dd
Phan-Thao <phanthao.2004t@gmail.com>
2025-03-14 16:18:35 +0700

    update salary

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap excel__1_ {
+            get {
+                object obj = ResourceManager.GetObject("excel (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap forgotpassword {
             get {
                 object obj = ResourceManager.GetObject("forgotpassword", resourceCulture);
